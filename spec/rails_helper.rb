2bc42144860c6a6613f0348185a1abfb2ce953f9
Armen <armen0089@gmail.com>
2018-02-23 02:20:51 +0400

    Added JWT Token an test cases

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,6 +72,10 @@ RSpec.configure do |config|
 
   config.include RequestSpecHelper, type: :request
 
+  config.include RequestSpecHelper
+
+  config.include ControllerSpecHelper
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
